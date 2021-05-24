ac6b977e49ced97d630aa4dff599c960bf5435ab
Noah <noahcallen@hotmail.com>
2021-05-23 17:12:17 -0700

    Fixed black bug

--- server.js
+++ server.js
@@ -109,10 +109,10 @@ function colorNameToHex(color) {
         "yellow": "#ffff00", "yellowgreen": "#9acd32", "orengeguy": "#ff6600", "drshadow": "#9a0000",
     };
 
-    if (typeof colors[color.toLowerCase()] != 'undefined') {
-        return colors[color.toLowerCase()];
-    } else if (color.toLowerCase() == "black") {
+    if (color.toLowerCase() == "black") {
         return colors[Object.Keys(colors)[Math.floor(Math.random() * 141)]]
+    } else if (typeof colors[color.toLowerCase()] != 'undefined') {
+        return colors[color.toLowerCase()];
     } else {
         return colors["blue"];
     }
